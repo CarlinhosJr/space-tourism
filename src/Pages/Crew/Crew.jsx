9313c3a3f4f6d8f970351b7261a1a7ff049ec761
graziosi <luizgraziosi@hotmail.com>
2022-10-13 17:48:39 -0300

    alteração

--- src/Pages/Crew/Crew.jsx
+++ src/Pages/Crew/Crew.jsx
@@ -8,7 +8,7 @@ const Crew = () =>{
     <div className="lg:bg-crew-desktop md:bg-crew-tablet bg-crew-mobile min-h-screen bg-cover bg-no-repeat"> 
         <Outlet/>
     </div>
-  );
+  )
 }
 
 export default Crew;
--- src/Pages/Technology/Tecnologia.jsx
+++ src/Pages/Technology/Tecnologia.jsx
@@ -6,7 +6,7 @@ const Technology = () =>{
     <div className="lg:bg-tech-desktop md:bg-tech-tablet bg-tech-mobile min-h-screen bg-cover bg-no-repeat"> 
       <Outlet/>
     </div>
-  );
+  )
 }
 
 export default Technology;
