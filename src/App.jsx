1c76d44bf00fcbac9c85bcac3d83c69ce337fc3f
graziosi <luizgraziosi@hotmail.com>
2022-10-14 13:13:49 -0300

    alteração

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './main.css'
 
-import Home from './pages/home/Index'
+import Home from './pages/home/Home'
 
 import Destination from './pages/destination/Destination'
 import Mars from './pages/destination/Marte'
